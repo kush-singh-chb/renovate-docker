07ef83618fea2ba4154c09fe3224fff09c43e315
kush-singh-chb <singh.kush100@gmail.com>
2025-01-29 11:25:43 +0000

    Renovate Docker

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    platform: "github",
+    autodiscover: true,
+    branchPrefix: 'renovate/',
+    onboarding: true,
+    onboardingConfig: {
+        "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+        "extends": [
+            "github>kush-singh-chb/renovatebot-selfhosted:kush-config.json"
+        ],
+    },
+    endpoint: 'https://api.github.com/',
+    token: process.env.GITHUB_COM_TOKEN ,
+//  "extends": ["config:recommended", ":skipStatusChecks"],
+};
